0213565a778cac0dd684700750753a308e05a110
Brad Morgan <jbradmorg16@gmail.com>
2023-12-31 12:18:23 -0500

    completed

---
+++ perimeter_area.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/*
+   Write a C program to compute the perimeter and area 
+   of a rectangle with a height of 7 inches and width of 5 inches.
+Expected Output:
+Perimeter of the rectangle = 24 inches
+Area of the rectangle = 35 square inches 
+*/
+
+int main() {
+    printf("     5    \n"); 
+    printf(" - - - - -\n");
+    printf("|         |\n");
+    printf("|         |\n");
+    printf("|         |\n");
+    printf("|         | 7\n");
+    printf("|         |\n");
+    printf("|         |\n");
+    printf("|         |\n");
+    printf("- - - - - -\n");
+
+
+    // printf("What is the perimeter of the rectangle?\n");
+    // printf("What is the area of the rectangle?\n");
+
+    int height = 7;
+    int width = 5;
+
+    int area = height * width;
+    int perimeter = height + height + width + width;
+
+    printf("The perimeter of the rectangle is: %d \n", perimeter);
+    printf("The area of the rectangle is: %d \n", area);    
+
+
+    return 0;
+    // This exercise is completed
+}
