2939b49be03cfe11ae518860083b99d5aa6a6ceb
Brad Morgan <jbradmorg16@gmail.com>
2024-01-01 21:30:22 -0500

    completed exercise

--- mult_variables.c
+++ mult_variables.c
@@ -27,8 +27,13 @@ int main()
     printf("a + c = %d \n", a + c);
     printf("x + c = %f \n", x + c);
     printf("dx + x = %f \n", dx + x);
-    printf("((int) dx) + ax = %ld", ((int) dx + ax));
-
+    printf("((int) dx) + ax = %ld \n", ((int) dx + ax));
+    printf("a + x = %lf \n", a + x);
+    printf("s + b = %d \n", s + b);
+    printf("ax + b = %ld \n", ax + b);
+    printf("s + c = %d \n", s + c);
+    printf("ax + c = %ld \n", ax + c);
+    printf("ax + ux = %lu \n", ax + ux);
 
     return 0;
 }
