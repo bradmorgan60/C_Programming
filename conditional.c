2045099eeb2e89c21dd56d3b6fb6251693149caa
Brad Morgan <jbradmorg16@gmail.com>
2024-01-12 05:37:14 -0500

    added an exercise

--- conditional.c
+++ conditional.c
@@ -19,18 +19,61 @@
 //     return 0;
 // }
 
-int main() {
+// int main() {
 
-    int num1;
+//     int num1;
 
-    printf("Enter an integer: ");
-    scanf("%d", &num1);
+//     printf("Enter an integer: ");
+//     scanf("%d", &num1);
 
-    if (num1 % 2 == 0)
-    {
-        printf("%d is even \n", num1);
-    } else printf("%d is odd \n", num1);
+//     if (num1 % 2 == 0)
+//     {
+//         printf("%d is even \n", num1);
+//     } else printf("%d is odd \n", num1);
     
 
+//     return 0;
+// }
+
+/*
+9. Write a C program to accept a coordinate point in an XY coordinate system and determine in which quadrant the coordinate point lies.
+Test Data : 7 9
+Expected Output :
+The coordinate point (7,9) lies in the First quadrant.
+2|1
+3|4
+*/
+
+int main() {
+
+    int num1, num2;
+
+    printf("Enter the first integer: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second integer: ");
+    scanf("%d", &num2);
+
+    if (num1 > 0 && num2 > 0)
+    {
+        printf("These are in Quadrant 1 \n");
+    }
+    else if (num1 > 0 && num2 < 0)
+    {
+        printf("Numbers are in Quadrant 4 \n");
+    }
+    else if (num1 < 0 && num2 > 0) 
+    {
+        printf("Numbers are in Quadrant 2 \n");
+    }
+    else printf("Numbers are in Qudrant 3 \n");
+    
+    
+
+
+
+
+
+    
     return 0;
 }
