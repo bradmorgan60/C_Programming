eba73fe0c15b0ab3d3ca65a8f6337956bb469eac
Brad Morgan <jbradmorg16@gmail.com>
2024-01-12 05:54:26 -0500

    completed another exercise

--- conditional.c
+++ conditional.c
@@ -44,36 +44,53 @@ The coordinate point (7,9) lies in the First quadrant.
 3|4
 */
 
+// int main() {
+
+//     int num1, num2;
+
+//     printf("Enter the first integer: ");
+//     scanf("%d", &num1);
+
+//     printf("Enter the second integer: ");
+//     scanf("%d", &num2);
+
+//     if (num1 > 0 && num2 > 0)
+//     {
+//         printf("These are in Quadrant 1 \n");
+//     }
+//     else if (num1 > 0 && num2 < 0)
+//     {
+//         printf("Numbers are in Quadrant 4 \n");
+//     }
+//     else if (num1 < 0 && num2 > 0) 
+//     {
+//         printf("Numbers are in Quadrant 2 \n");
+//     }
+//     else printf("Numbers are in Qudrant 3 \n");
+    
+//     return 0;
+// }
+
 int main() {
-
-    int num1, num2;
-
-    printf("Enter the first integer: ");
+    int num1, num2, num3;
+    printf("Welcome this program. Enter the sides of your triangle.\n");
+    printf("Enter side 1: ");
     scanf("%d", &num1);
 
-    printf("Enter the second integer: ");
+    printf("Enter side 2: ");
     scanf("%d", &num2);
 
-    if (num1 > 0 && num2 > 0)
-    {
-        printf("These are in Quadrant 1 \n");
+    printf("Enter side 3: ");
+    scanf("%d", &num3);
+
+    if (num1 == num2 && num2 == num3) {
+        printf("Equilateral Triangle");
     }
-    else if (num1 > 0 && num2 < 0)
-    {
-        printf("Numbers are in Quadrant 4 \n");
+    else if (num1 == num2 || num1 == num3 || num2 == num3) {
+        printf("Isoceles Triangle");
     }
-    else if (num1 < 0 && num2 > 0) 
-    {
-        printf("Numbers are in Quadrant 2 \n");
-    }
-    else printf("Numbers are in Qudrant 3 \n");
-    
-    
+    else printf("Scalene triangle");
 
 
-
-
-
-    
     return 0;
 }
